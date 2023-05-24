2f2acb4362e68f813e525d167c630001e42e94e2
Luis Francisco Flores Robles <fl337789@uaeh.edu.mx>
2023-05-23 22:44:42 -0600

    Realice los 3 ejercicios de variables del curso

---
+++ Dia2/variables.py
@@ -0,0 +1,15 @@
+nombre = "juan"
+print(nombre)
+
+nombre = "maria"
+print(nombre)
+
+concatenar = "hola " + nombre
+print(concatenar)
+
+edad = input("Cuantos años tienes: ")
+print("en 10 años tendras: " + edad)
+
+numero1 = 10
+numero2 = 22
+print(numero2 + numero1)
---
+++ Dia2/variables1.ejercicio.py
@@ -0,0 +1,7 @@
+# Práctica con Variables 1 Declara dos variables, llamadas nombre y edad. Asigna a la variable nombre el valor
+# Tony Soprano", y a la edad, el valor 51.
+
+nombre = "Tony Soprano"
+edad = 51
+
+print("mi nombre es " + nombre + "y tengo " + str(edad) + " años")
